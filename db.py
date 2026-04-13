14d9f7dae8c72076880a75a436e83e0cd924ffd4
joserodrigues092010-web <joserodrigues092010@gmail.com>
2026-04-13 09:01:15 +0100

    Mudanças new

---
+++ db.py
@@ -0,0 +1,33 @@
+import sqlite3
+conn = sqlite3.connect("jogo_palavras.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS palavras (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    palavra TEXT NOT NULL UNIQUE
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS moedas (
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    quantidade INTEGER NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS login (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+)
+""")
+
+
+cursor.execute("INSERT OR IGNORE INTO moedas (id, quantidade) VALUES (1, 0)")
+conn.commit()
+
+
+cursor.execute("SELECT quantidade FROM moedas WHERE id = 1")
+moedas = cursor.fetchone()[0]
--- joguito_simples.py
+++ joguito_simples.py
@@ -1,58 +1,44 @@
 import time
+
 import pygame
 import sqlite3
+import os
+import sys
+import getpass
 from colorama import Fore, Style, init
 init(autoreset=True)
 
+import db
 
-pygame.mixer.init()  
+pygame.mixer.init()
+  
 som_erro = pygame.mixer.Sound("erro.mp3")
 dinheirao_ganhar = pygame.mixer.Sound("para-sesi-efekti_PaUswM1.mp3")
 
-
+pygame.mixer.music.load("Nintendo 3DS Internet Settings Theme (High Quality, 2022 Remastered).mp3")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.1)
+logindef = True
+admin = True
+logado = True
+criar_conta = False
+adminpass = True
+admin_power = False
 conn = sqlite3.connect("jogo_palavras.db")
 cursor = conn.cursor()
 
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS palavras (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    palavra TEXT NOT NULL UNIQUE
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS moedas (
-    id INTEGER PRIMARY KEY CHECK (id = 1),
-    quantidade INTEGER NOT NULL
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS login (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-)
-""")
-
-
-cursor.execute("INSERT OR IGNORE INTO moedas (id, quantidade) VALUES (1, 0)")
-conn.commit()
-
-
+# --- INICIALIZAR MOEDAS ---
 cursor.execute("SELECT quantidade FROM moedas WHERE id = 1")
-moedas = cursor.fetchone()[0]
+resultado_moedas = cursor.fetchone()
+if resultado_moedas:
+    moedas = resultado_moedas[0]
+else:
+    moedas = 0
+    cursor.execute("INSERT OR IGNORE INTO moedas (id, quantidade) VALUES (1, ?)", (moedas,))
+    conn.commit()
+# --------------------------
 
-
-pygame.mixer.music.load("Nintendo 3DS Internet Settings Theme (High Quality, 2022 Remastered).mp3")
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(0.3)
-
-logao = True
-criar_conta = False
-
-while logao:
+while logado:
     print(Style.BRIGHT + Fore.CYAN + "="*50)
     print(Style.BRIGHT + Fore.MAGENTA + "         BEM-VINDO AO JOGO DAS PALAVRAS!  ")
     print(Style.BRIGHT + Fore.CYAN + "="*50)
@@ -64,17 +50,132 @@ while logao:
         opcaodelogin = int(input("Digite opção: "))
     except ValueError:
         print("Escreve apenas números!")
+        som_erro.play()
+        som_erro.set_volume(0.1)
         continue
 
     if opcaodelogin == 1:
         criar_conta = True
-        logao = False
+        logado = False
+        
     elif opcaodelogin == 2:
         criar_conta = False
-        logao = False
+        logado = False
+    elif opcaodelogin == 3:
+        while admin:
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load("Gran Turismo 6 Soundtrack - annayamada - 4 CHORDS [sT8x_MIgCOE].mp3")
+            pygame.mixer.music.play(-1)
+            pygame.mixer.music.set_volume(0.1)
+            print("ADMIN MODE")
+            logadm = input("Nome Do Adm: ")
+            if logadm == "Admin123":
+                print("Nome Correspondido")
+            
+            else:
+                print("Nome Não Correspondido")
+                som_erro.play()
+                som_erro.set_volume(0.1)
+                continue
+            
+            while adminpass:
+                passadm = getpass.getpass("Pass do Adm ")
+                if passadm == "123123adm":
+                    admin = False
+                    logado = False
+                    adminpass = False
+                    logindef = False
+                    admin_power = True
+                    break
+                    
+                    
+                else:
+                    print("Palavra Pass Não Correspondida")
+                    continue
+            
+        
     else:
         print("Opção inválida!")
+        som_erro.play()
+        som_erro.set_volume(0.1)
+        
+        
 
+if admin_power:
+    while True:
+        print("MODO ADMIN ATIVADO!")
+        print("Digite '1' para eliminar uma conta")
+        print("Digite '2' para sair do modo admin")
+        print("Digite '3' para mostrar contas criadas")
+        print("Digite '4' para apagar todas as moedas")
+        print("Digite '5' para apagar todas as contas")
+        print("Digite '6' para apagar todas as palavras")
+        palavra = input("Digite aqui: ")
+        
+        if palavra == "1":
+            nome = input("Digite o nome da conta a apagar: ")
+            password = input("Digite a password: ")
+
+            cursor.execute("SELECT * FROM login WHERE nome = ?", (nome,))
+            resultado = cursor.fetchone()
+
+            if not resultado:
+                print("Conta não encontrada!")
+                som_erro.play()
+                som_erro.set_volume(0.1)
+
+            elif password != resultado[2]:
+                print("Password incorreta!")
+                som_erro.play()
+                som_erro.set_volume(0.1)
+
+            else:
+                cursor.execute("DELETE FROM login WHERE nome = ?", (nome,))
+                conn.commit()
+                print("Conta apagada com sucesso!")
+
+        elif palavra == "2":
+            admin_power = False
+            logindef = True
+            break
+         
+            
+        elif palavra == "3":
+            cursor.execute("SELECT nome, password FROM login")
+            contas = cursor.fetchall()
+
+            print("\nContas registadas:")
+            for conta in contas:
+                print("Nome:", conta[0], "| Password:", conta[1])
+        
+        elif palavra == "4":
+          
+            moedas = 0
+            cursor.execute("UPDATE moedas SET quantidade = 0 WHERE id = 1")
+            conn.commit()
+            print("Moedas resetadas com sucesso!")
+        
+        elif palavra == "5":
+        
+            cursor.execute("DELETE FROM login")
+            conn.commit()
+            print("Todas as contas foram apagadas!")
+        
+        elif palavra == "6":
+            
+            cursor.execute("DELETE FROM palavras")
+            conn.commit()
+            print("Todas as palavras foram apagadas!")
+   
+            
+
+
+        else:
+            print("opção invalida!")
+            som_erro.play()
+            som_erro.set_volume(0.1)
+            continue
+        
 
 if criar_conta:
     while True:
@@ -85,8 +186,9 @@ if criar_conta:
             som_erro.play()
             som_erro.set_volume(0.1)
             continue
+        
 
-        password = input("Crie sua palavra pass: ")
+        password = getpass.getpass("Crie sua palavra pass: ")
         try:
             cursor.execute("INSERT INTO login (nome, password) VALUES (?, ?)", (login, password))
             conn.commit()
@@ -98,17 +200,21 @@ if criar_conta:
             som_erro.set_volume(0.1)
 
 
-while True:
+while logindef:
     login1 = input("Nome da conta: ")
+
+   
+
     cursor.execute("SELECT * FROM login WHERE nome = ?", (login1,))
     resultado = cursor.fetchone()
+
     if not resultado:
         print("Conta não encontrada!")
         som_erro.play()
         som_erro.set_volume(0.1)
         continue
 
-    password1 = input("Password: ")
+    password1 = getpass.getpass("Password: ")
     if password1 != resultado[2]:  
         print("Password incorreta!")
         som_erro.play()
@@ -146,6 +252,10 @@ while perguntar:
         print(Fore.GREEN + "Moedas:", moedas)
         dinheirao_ganhar.play()
         dinheirao_ganhar.set_volume(0.6)
+    elif palavra == "sair":
+        perguntar = False
+        print(Fore.RED + "Programa terminado." + Style.RESET_ALL)
+        print("Moedas ganhas:", Fore.YELLOW + str(moedas) + Style.RESET_ALL)
 
     else:
         cursor.execute("SELECT palavra FROM palavras WHERE palavra = ?", (palavra,))
@@ -157,6 +267,7 @@ while perguntar:
             print("Desculpa, mas não conheço.")
             addpala = input("Quer adicionar essa palavra?" + Fore.RED + Style.BRIGHT + " (sim/nao) " + Style.RESET_ALL).strip().lower()
             if addpala == "sim":
+                
                 try:
                     cursor.execute("INSERT INTO palavras (palavra) VALUES (?)", (palavra,))
                     moedas += 1
@@ -168,8 +279,7 @@ while perguntar:
                 except sqlite3.IntegrityError:
                     print("Essa palavra já existe!")
             elif addpala == "nao":
-                perguntar = False
-                print(Fore.RED + "Programa terminado." + Style.RESET_ALL)
-                print("Moedas ganhas:", Fore.YELLOW + str(moedas) + Style.RESET_ALL)
+                continue
+            
 
 conn.close()
